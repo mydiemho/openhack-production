14a17daeaf4caec204d24b73b8ab2bd9060136d9
My Ho <mydiemho@users.noreply.github.com>
2020-01-22 17:57:52 -0800

    debugging

--- deployment/arm/deploy-region.sh
+++ deployment/arm/deploy-region.sh
@@ -71,21 +71,27 @@ echo "Environment: $env"
 echo "Region: $region"
 echo
 
-echo "Creating Regional Resource Group: $resourceGroupName"
+echo "######## Creating Regional Resource Group: $resourceGroupName"
 az group create \
     --name $resourceGroupName \
     --location $region
+
+echo 
+echo "Sleeping for a bit to wait for resource grupo creation to finish ...."
+sleep 10
+
 echo
-echo "Deploying regional resources to $resourceGroupName"
+echo "######## Deploying regional resources to $resourceGroupName"
 az group deployment create \
     --name "Urlist-$region-$(timestamp)" \
     --resource-group $resourceGroupName \
     --template-file region.json \
     --parameters location=$region apimName=$apimName appServicePlanName=$appServicePlanName \
-    backendAppName=$backendAppName appInsightsName=$appInsightsName storageActName=$storageActName
+    backendAppName=$backendAppName appInsightsName=$appInsightsName storageActName=$storageActName \
+    --verbose
 
 echo
-echo "Configuring blob storage for static website hosting"
+echo "######## Configuring blob storage for static website hosting"
 az storage blob service-properties update \
     --account-name $storageActName \
     --static-website \
